d8c0f72d6868643f4f6d131a05a6d93c792334bb
Will <changwei.cn@gmail.com>
2016-09-14 10:21:29 +0800

    tested with group by two dims

--- src/test/java/com/github/myretail/sql4es/esjdbc/MySimpleSelectsIT.java
+++ src/test/java/com/github/myretail/sql4es/esjdbc/MySimpleSelectsIT.java
@@ -2,35 +2,54 @@ package com.github.myretail.sql4es.esjdbc;
 
 import nl.anchormen.esjdbc.Sql4EsBase;
 import nl.anchormen.sql4es.model.Utils;
+import org.elasticsearch.SpecialPermission;
 import org.elasticsearch.test.ESIntegTestCase.ClusterScope;
 import org.elasticsearch.test.ESIntegTestCase.Scope;
 import org.junit.Test;
 
 import java.sql.*;
 
-@ClusterScope(scope=Scope.TEST, numDataNodes=1)
-public class MySimpleSelectsIT extends Sql4EsBase {
+
+public class MySimpleSelectsIT  {
 
 	private String index = "item_sku_income";
 	private String type = "sku_income";
 
+	private SecurityManager sm;
+
+	/**
+	 * Loads the ESDriver
+	 * @throws Exception
+	 */
 	public MySimpleSelectsIT() throws Exception {
 		super();
+		Class.forName("nl.anchormen.sql4es.jdbc.ESDriver");
+		this.sm = System.getSecurityManager();
+		if (sm != null) {
+			// unprivileged code such as scripts do not have SpecialPermission
+			sm.checkPermission(new SpecialPermission());
+		}
 	}
-	
+
+
 
 	@Test
 	public void selectAll() throws Exception{
-//		createIndexTypeWithDocs(index, type, true, 10);
+
+		String sql ="select sum(sale_amount) as sale_amount, item_second_cate_name, item_third_cate_name from sku_income group by item_second_cate_name, item_third_cate_name";
+
+//		sql="select * from "+type+" limit 5";
+
 		Statement st = DriverManager.getConnection("jdbc:sql4es://es.test.standino.com:9303/"+index+"?cluster.name=jiesi-1").createStatement();
-		ResultSet rs = st.executeQuery("select * from "+type+" limit 5");
+		ResultSet rs = st.executeQuery(sql);
 		ResultSetMetaData rsm = rs.getMetaData();
 
-		
+
+
 		int count = 0;
 		while(rs.next()){
 			count++;
-
+            System.out.print(rs.getObject("item_second_cate_name")+"----"+rs.getObject("item_third_cate_name")+":  ");
 			System.out.println(rs.getObject("sale_amount"));
 
 		}
